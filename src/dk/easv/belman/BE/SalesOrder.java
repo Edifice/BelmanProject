fa29ab433fef10dcfbc4fd16d6cef5e71727bf5e
Edifice <salidani@gmail.com>
2013-04-29 11:51:34 +0200

    SalesOrder model refs #26

--- src/dk/easv/belman/BE/SalesOrder.java
+++ src/dk/easv/belman/BE/SalesOrder.java
@@ -1,6 +1,10 @@
 package dk.easv.belman.BE;
 
 public class SalesOrder extends IEntity {
+    
+    private int orderId;
+    private String orderDescription;
+    private boolean isDone;
 
     @Override
     public void save() {
@@ -10,5 +14,47 @@ public class SalesOrder extends IEntity {
     @Override
     public void update() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * @return the orderId
+     */
+    public int getOrderId() {
+        return orderId;
+    }
+
+    /**
+     * @param orderId the orderId to set
+     */
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    /**
+     * @return the orderDescription
+     */
+    public String getOrderDescription() {
+        return orderDescription;
+    }
+
+    /**
+     * @param orderDescription the orderDescription to set
+     */
+    public void setOrderDescription(String orderDescription) {
+        this.orderDescription = orderDescription;
+    }
+
+    /**
+     * @return the isDone
+     */
+    public boolean isDone() {
+        return isDone;
+    }
+
+    /**
+     * @param isDone the isDone to set
+     */
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
     }
 }
