6d165ca321a01cff0d35cdf2165eb2edf11f08cd
Martin Rasmussen <martinrasmussen92@gmail.com>
2013-04-30 12:55:31 +0200

    refs #[9] - Tree is now showing all orders - Made BLL for adding database data into different lists

--- src/dk/easv/belman/UI/MyTreeTableModel.java
+++ src/dk/easv/belman/UI/MyTreeTableModel.java
@@ -1,80 +1,94 @@
 package dk.easv.belman.UI;
 
+import dk.easv.belman.BE.BList;
+import dk.easv.belman.BE.Item;
+import dk.easv.belman.BE.ItemList;
+import dk.easv.belman.BE.ProductOrder;
+import dk.easv.belman.BE.ProductOrderList;
+import dk.easv.belman.BE.SalesOrder;
+import dk.easv.belman.BE.SalesOrderList;
+import dk.esav.belman.BLL.ListManager;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.jdesktop.swingx.treetable.AbstractTreeTableModel;
 
 public class MyTreeTableModel extends AbstractTreeTableModel {
-
+    
     private MyTreeNode root;
-
+    private ListManager allLists;
+    
     public MyTreeTableModel() {
+        init();
         populateTable();
-
-
+        
+        
     }
-
+    
+    private void init() {
+        allLists = new ListManager();
+        
+    }
+    
     private void populateTable() {
-        ArrayList<SalesOrder> sOrders = new ArrayList<>();
-        ArrayList<ProductionOrder> pOrders = new ArrayList<>();
-        ArrayList<Item> items = new ArrayList<>();
-
-        root = new MyTreeNode("root", "description");
+        
+        
+        root = new MyTreeNode();
 
 
-        for (int i = 0; i < 4; i++) {
-            sOrders.add(new SalesOrder(i));
-        }
-        pOrders.add(new ProductionOrder(0, 0, "01-01-2013"));
-        pOrders.add(new ProductionOrder(1, 1, "02-02-2013"));
-        pOrders.add(new ProductionOrder(2, 1, "03-02-2013"));
-        pOrders.add(new ProductionOrder(3, 3, "03-02-2013"));
-        pOrders.add(new ProductionOrder(4, 4, "03-02-2013"));
-        pOrders.add(new ProductionOrder(4, 2, "03-02-2013"));
+//        for (int i = 0; i < 4; i++) {
+//            sOrders.add(new SalesOrder(i));
+//        }
+//        pOrders.add(new ProductionOrder(0, 0, "01-01-2013"));
+//        pOrders.add(new ProductionOrder(1, 1, "02-02-2013"));
+//        pOrders.add(new ProductionOrder(2, 1, "03-02-2013"));
+//        pOrders.add(new ProductionOrder(3, 3, "03-02-2013"));
+//        pOrders.add(new ProductionOrder(4, 4, "03-02-2013"));
+//        pOrders.add(new ProductionOrder(4, 2, "03-02-2013"));
+//
+//        items.add(new Item("10", 1, 300, 400, 0.22, 30));
+//        items.add(new Item("15", 1, 500, 400, 0.22, 30));
+//        items.add(new Item("12", 2, 800, 400, 0.22, 30));
+//        items.add(new Item("5", 2, 800, 400, 0.22, 30));
+//        items.add(new Item("7", 0, 800, 400, 0.22, 30));
+//        items.add(new Item("8", 3, 800, 400, 0.22, 30));
+//        items.add(new Item("333", 4, 800, 400, 0.22, 30));
+//        items.add(new Item("40", 4, 800, 400, 0.22, 30));
 
-        items.add(new Item("10", 1, 300, 400, 0.22, 30));
-        items.add(new Item("15", 1, 500, 400, 0.22, 30));
-        items.add(new Item("12", 2, 800, 400, 0.22, 30));
-        items.add(new Item("5", 2, 800, 400, 0.22, 30));
-        items.add(new Item("7", 0, 800, 400, 0.22, 30));
-        items.add(new Item("8", 3, 800, 400, 0.22, 30));
-        items.add(new Item("333", 4, 800, 400, 0.22, 30));
-        items.add(new Item("40", 4, 800, 400, 0.22, 30));
-
-        for (SalesOrder s : sOrders) {
+        for (SalesOrder s : allLists.getAllSalesOrder().getList()) {
             //Uncomment this to set ID as name
             //String sName = String.valueOf(s.getId());
 
             //Change to sName;
-            MyTreeNode sRoot = new MyTreeNode("SalesOrder", "Description");
-
+            MyTreeNode sRoot = new MyTreeNode(s.getId(), s.getDescription());
+            
             root.getChildren().add(sRoot);
-
-            for (ProductionOrder p : pOrders) {
+            
+            for (ProductOrder p : allLists.getAllProductOrder(s.getId()).getList()) {
                 //Uncomment this to set ID as name
                 //String pName = String.valueOf(p.getId());
 
                 //Change first param to pName;
-                MyTreeNode pRoot = new MyTreeNode("ProductionOrder", "Description", p.getDueDate());
-                if (s.getId() == p.getSId()) {
-                    sRoot.getChildren().add(pRoot);
-                }
+                MyTreeNode pRoot = new MyTreeNode(p.getId(), p.getDescription(), p.getDueDate());
+                //if (s.getId() == p.getSId()) {
+                sRoot.getChildren().add(pRoot);
+                //}
 
-                for (Item item : items) {
-                    if (p.getId() == item.getOrdId()) {
-                        pRoot.getChildren().add(new MyTreeNode("Item", item.getMatId(), item.getLength(), item.getWidth(), item.getCircumference(), item.getQuantity()));
-                    }
+                for (Item item : allLists.getAllItems(p.getId()).getList()) {
+                    //if (p.getId() == item.getOrdId()) {
+                    pRoot.getChildren().add(new MyTreeNode(item.getId(), item.getMaterialId(), item.getWidth(), item.getCircumference(), item.getThickness(), item.getQuantity()));
+                    //}
                 }
             }
         }
     }
-
+    
     @Override
     public int getColumnCount() {
         return 8;
     }
-
+    
     @Override
     public String getColumnName(int column) {
         switch (column) {
@@ -87,47 +101,55 @@ public class MyTreeTableModel extends AbstractTreeTableModel {
             case 3:
                 return "Material ID";
             case 4:
-                return "Length";
-            case 5:
                 return "Width";
-            case 6:
+            case 5:
                 return "Circumferance";
+            case 6:
+                return "Thickness";
             case 7:
                 return "Quantity";
             default:
                 return "Unknown";
         }
     }
-
+    
     @Override
     public Object getValueAt(Object node, int column) {
         //System.out.println("getValueAt: " + node + ", " + column);
         MyTreeNode treenode = (MyTreeNode) node;
         switch (column) {
-
+            
             case 0:
-                return treenode.getName();
+                return treenode.getId();
             case 1:
                 return treenode.getDescription();
             case 2:
-                return treenode.getDueDate();
-            case 3:
-                return treenode.getMatId();
-            case 4:
-                if (treenode.getLength() != 0) {
-                    return treenode.getLength();
+                if (treenode.getDueDate() != 0) {
+                    return new Timestamp(treenode.getDueDate());
                 } else {
                     return "";
                 }
-            case 5:
+            case 3:
+                if (treenode.getMatId() != 0) {
+                    return treenode.getMatId();
+                } else {
+                    return "";
+                }
+            case 4:
                 if (treenode.getWidth() != 0) {
                     return treenode.getWidth();
                 } else {
                     return "";
                 }
-            case 6:
+            case 5:
                 if (treenode.getCircumference() != 0) {
                     return treenode.getCircumference();
+                } else {
+                    return "";
+                }
+            case 6:
+                if (treenode.getThickness() != 0) {
+                    return treenode.getThickness();
                 } else {
                     return "";
                 }
@@ -140,21 +162,21 @@ public class MyTreeTableModel extends AbstractTreeTableModel {
             default:
                 return "Unknown";
         }
-
+        
     }
-
+    
     @Override
     public Object getChild(Object node, int index) {
         MyTreeNode treenode = (MyTreeNode) node;
         return treenode.getChildren().get(index);
     }
-
+    
     @Override
     public int getChildCount(Object parent) {
         MyTreeNode treenode = (MyTreeNode) parent;
         return treenode.getChildren().size();
     }
-
+    
     @Override
     public int getIndexOfChild(Object parent, Object child) {
         MyTreeNode treenode = (MyTreeNode) parent;
@@ -167,7 +189,7 @@ public class MyTreeTableModel extends AbstractTreeTableModel {
         // TODO Auto-generated method stub
         return 0;
     }
-
+    
     public boolean isLeaf(Object node) {
         MyTreeNode treenode = (MyTreeNode) node;
         if (treenode.getChildren().size() > 0) {
@@ -175,7 +197,7 @@ public class MyTreeTableModel extends AbstractTreeTableModel {
         }
         return true;
     }
-
+    
     @Override
     public Object getRoot() {
         return root;
@@ -183,159 +205,164 @@ public class MyTreeTableModel extends AbstractTreeTableModel {
 }
 
 class MyTreeNode {
-
+    
     private List<MyTreeNode> children = new ArrayList<>();
-    private String name, dueDate, matId, description;
-    private double length, width, circumference;
-    private int quantity;
-
+    private String description, name;
+    private double thickness, width, circumference;
+    private int quantity, id, matId;
+    private long dueDate;
+    
     public MyTreeNode() {
     }
     //Sales order node
 
-    public MyTreeNode(String name, String description) {
-        this.name = name;
+    public MyTreeNode(int id, String description) {
+        this.id = id;
         this.description = description;
-
-
+        
+        
     }
     //Production order node
 
-    public MyTreeNode(String name, String description, String dueDate) {
-        this.name = name;
+    public MyTreeNode(int id, String description, long dueDate) {
+        this.id = id;
         this.description = description;
         this.dueDate = dueDate;
     }
+    
     //Item order node
-
-    public MyTreeNode(String name, String matId, double length, double width, double circumference, int quantity) {
-        this.name = name;
+    public MyTreeNode(int id, int matId, double width, double thickness, double circumference, int quantity) {
+        
         this.matId = matId;
-        this.length = length;
+        this.id = id;
         this.width = width;
         this.circumference = circumference;
         this.quantity = quantity;
+        this.thickness = thickness;
     }
-
+    
+    public int getId() {
+        return id;
+    }
+    
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
+    
+    public double getThickness() {
+        return thickness;
     }
-
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
     public List<MyTreeNode> getChildren() {
         return children;
     }
-
-    public String getMatId() {
+    
+    public int getMatId() {
         return matId;
     }
-
-    public double getLength() {
-        return length;
-    }
-
-    public String getDueDate() {
+    
+    public long getDueDate() {
         return dueDate;
     }
-
+    
     public String getDescription() {
         return description;
     }
-
+    
     public double getWidth() {
         return width;
     }
-
+    
     public double getCircumference() {
         return circumference;
     }
-
+    
     public int getQuantity() {
         return quantity;
     }
-
+    
     public String toString() {
-        return "MyTreeNode: " + name;
+        return "MyTreeNode: " + id;
     }
 }
-
-class SalesOrder {
-
-    private int id;
-
-    public SalesOrder(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
-}
-
-class ProductionOrder {
-
-    private int id;
-    private int sId;
-    private String dueDate;
-
-    public ProductionOrder(int id, int sId, String dueDate) {
-        this.id = id;
-        this.sId = sId;
-        this.dueDate = dueDate;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public int getSId() {
-        return sId;
-    }
-
-    public String getDueDate() {
-        return dueDate;
-    }
-}
-
-class Item {
-
-    private String matId;
-    private int ordId, quantity;
-    private double length, width, circumference;
-
-    public Item(String matId, int ordId, double length, double width, double circumference, int quantity) {
-        this.matId = matId;
-        this.ordId = ordId;
-        this.length = length;
-        this.width = width;
-        this.circumference = circumference;
-        this.quantity = quantity;
-    }
-
-    public String getMatId() {
-        return matId;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public double getCircumference() {
-        return circumference;
-    }
-
-    public int getOrdId() {
-        return ordId;
-    }
-
-    public double getLength() {
-        return length;
-    }
-}
+//class SalesOrder {
+//
+//    private int id;
+//
+//    public SalesOrder(int id) {
+//        this.id = id;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//}
+//
+//class ProductionOrder {
+//
+//    private int id;
+//    private int sId;
+//    private String dueDate;
+//
+//    public ProductionOrder(int id, int sId, String dueDate) {
+//        this.id = id;
+//        this.sId = sId;
+//        this.dueDate = dueDate;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public int getSId() {
+//        return sId;
+//    }
+//
+//    public String getDueDate() {
+//        return dueDate;
+//    }
+//}
+//
+//class Item {
+//
+//    private String matId;
+//    private int ordId, quantity;
+//    private double length, width, circumference;
+//
+//    public Item(String matId, int ordId, double length, double width, double circumference, int quantity) {
+//        this.matId = matId;
+//        this.ordId = ordId;
+//        this.length = length;
+//        this.width = width;
+//        this.circumference = circumference;
+//        this.quantity = quantity;
+//    }
+//
+//    public String getMatId() {
+//        return matId;
+//    }
+//
+//    public int getQuantity() {
+//        return quantity;
+//    }
+//
+//    public double getWidth() {
+//        return width;
+//    }
+//
+//    public double getCircumference() {
+//        return circumference;
+//    }
+//
+//    public int getOrdId() {
+//        return ordId;
+//    }
+//
+//    public double getLength() {
+//        return length;
+//    }
+// }
---
+++ src/dk/esav/belman/BLL/ListManager.java
@@ -0,0 +1,68 @@
+package dk.esav.belman.BLL;
+
+import dk.easv.belman.BE.ItemList;
+import dk.easv.belman.BE.ProductOrderList;
+import dk.easv.belman.BE.SalesOrderList;
+import dk.easv.belman.DAL.DBConnection;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ListManager {
+
+    DBConnection myConnection;
+
+    public ListManager() {
+        try {
+            myConnection = DBConnection.getInstance();
+        } catch (SQLException ex) {
+            Logger.getLogger(ListManager.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ListManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public SalesOrderList getAllSalesOrder() {
+        try {
+            return myConnection.getAllSalesOrder();
+        } catch (SQLException ex) {
+            Logger.getLogger(ListManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    public ProductOrderList getAllProductOrder() {
+        try {
+            return myConnection.getAllProductionOrder();
+        } catch (SQLException ex) {
+            Logger.getLogger(ListManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    public ProductOrderList getAllProductOrder(int salesOrderId) {
+        try {
+            return myConnection.getAllProductionOrder(salesOrderId);
+        } catch (SQLException ex) {
+            Logger.getLogger(ListManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;    
+    }
+    public ItemList getAllItem() {
+        try {
+            return myConnection.getAllItem();
+        } catch (SQLException ex) {
+            Logger.getLogger(ListManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    public ItemList getAllItems(int productionOrderId) {
+        try {
+            return myConnection.getAllItem(productionOrderId);
+        } catch (SQLException ex) {
+            Logger.getLogger(ListManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
